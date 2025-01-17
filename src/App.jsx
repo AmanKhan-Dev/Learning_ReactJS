26249e34d4e2bd5be25255e24dbe8171f9b3207c
AmanKhan-Dev <amankhan7058int@gmail.com>
2025-01-17 11:13:51 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Card from './Card';
+import Form from './Form';
+
+function App() {
+ 
+  return (
+    <>
+   <Card/>
+   <br />
+   <br />
+   <br />
+   <Form/>
+
+  
+   </>
+
+   
+  );
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+import "./Card.css"
+
+
+function Card(){
+
+    return(
+     <div id="divtag1">
+      <h1 id="head1">THE BIG HEAD TAG</h1>
+       <h3>head 1</h3>
+       <h3>head 2</h3>
+       <h3>head 3</h3>
+     </div>
+
+
+
+    );
+}
+export default Card;
