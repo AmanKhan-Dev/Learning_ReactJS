9a9332a00468094f20d9e77679ad40cdb97b688a
AmanKhan-Dev <amankhan7058int@gmail.com>
2025-01-20 20:47:18 +0530

    Using React Hook

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import Button from '@mui/material/Button'
 
 function Counter(){
 const [count,setCount] = useState(0)
 
+const[name,setname] = useState();
 const increment = () => setCount(count +1);
 const decrement = () =>setCount(count -1);
 const reset = () => setCount(0);
 
-
-
+const setUsername = () => {
+    const newName = window.prompt("Enter your Name:");
+    if (newName) {
+      setname(`Good Morning, ${newName}`);
+    }
+  };
 return(
     <>
 <h1>{count}</h1>
@@ -21,7 +27,11 @@ return(
       <button onClick={reset} type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
             Reset
       </button>
-
+      
+<h2>{name}</h2>
+<Button onClick={setUsername} variant="text" color="default">
+  SET NAME
+</Button>
 
 
 </>
